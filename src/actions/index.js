5cce0837991be24bddc2b144240ff1b6be82f192
YuraKolodiy13 <Timberwolves13>
2019-08-21 17:38:48 +0300

    work more

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import axios from '../axios/axios-quiz'
-import {FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS} from "./actionType";
+import {
+  FETCH_QUIZ_SUCCESS,
+  FETCH_QUIZES_ERROR,
+  FETCH_QUIZES_START,
+  FETCH_QUIZES_SUCCESS, FINISH_QUIZ, QUIZ_NEXT_QUESTION, QUIZ_RETRY,
+  QUIZ_SET_STATE
+} from "./actionType";
 
 export const fetchQuizes = () => {
   return async dispatch => {
@@ -22,6 +28,29 @@ export const fetchQuizes = () => {
   }
 };
 
+export const fetchQuizById = quizId => {
+  return async  dispatch => {
+    dispatch(fetchQuizesStart());
+
+    try {
+      const response = await axios.get(`/quizes/${quizId}.json`);
+      const quiz = response.data;
+
+      dispatch(fetchQuizSuccess(quiz))
+
+    }catch (e) {
+      dispatch(fetchQuizesError(e));
+    }
+  }
+};
+
+export const fetchQuizSuccess = quiz => {
+  return{
+    type: FETCH_QUIZ_SUCCESS,
+    quiz
+  }
+};
+
 export const fetchQuizesStart = () => {
   return{
     type: FETCH_QUIZES_START
@@ -41,3 +70,72 @@ export const fetchQuizesError = (e) => {
     error: e
   }
 };
+
+export const quizSetState = (answerState, results) => {
+  return{
+    type: QUIZ_SET_STATE,
+    answerState,
+    results
+  }
+};
+
+export const finishQuiz = () => {
+  return{
+    type: FINISH_QUIZ
+  }
+};
+export const quizNextQuestion = (number) => {
+  return{
+    type: QUIZ_NEXT_QUESTION,
+    number
+  }
+};
+
+export const retryQuiz = () => {
+  return{
+    type: QUIZ_RETRY
+  }
+};
+
+
+export const quizAnswerClick = answerId => {
+  return (dispatch, getState) => {
+    const state = getState().quiz;
+    if(state.answerState){
+      const key = Object.keys(state.answerState)[0];
+      if(state.answerState[key] === 'success'){
+        return
+      }
+    }
+
+    const question = state.quiz[state.activeQuestion];
+    const results = state.results;
+
+    if(question.rightAnswerId === answerId){
+
+      if(!results[question.id]){
+        results[question.id] = 'success'
+      }
+      dispatch(quizSetState({[answerId]: 'success'}, results));
+
+      const timeout = window.setTimeout(() => {
+        if(isQuizFinished(state)){
+          dispatch(finishQuiz())
+        }else{
+          dispatch(quizNextQuestion(state.activeQuestion + 1))
+        }
+
+        window.clearTimeout(timeout);
+      }, 500);
+    }else {
+      results[question.id] = 'error';
+
+      dispatch(quizSetState({[answerId]: 'error'}, results));
+    }
+  }
+};
+
+const isQuizFinished = state => {
+  return state.activeQuestion + 1 === state.quiz.length
+}
+
